1f212b488f4f91494a673c8cbb6dcdeb958a728d
Andrea <andrea.pozza@hotmail.com>
2020-06-11 01:20:11 +0100

    slides aggiunte via javascript

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,38 @@ $('#carouselExampleCaptions').on('slide.bs.carousel', function() {
         video_player.attr('src', url);
     })
 })
+var archivio = {
+    "video": [{
+            "titolo": "LA CASA DI CARTA, TOKYO - Stag 1. / Ep. 6",
+            "descrizione": "La casa di carta è una delle serie più viste su netflix Italia e ha avuto un successo mondiale pazzesco. A parer mio non è mai scontata, nonostante la trama segua sempre lo schema della prima stagione, non manca mai d’originalità ed è ben condita da colpi di scena. Per questo motivo la 3ª e la 4ª sono le mie stagioni preferite (anche se la morte di Nairobi è stata devastante☹️). Tuttavia io ho scelto di riproporvi questa scena, che è della 1ª stagione, non solo perché Tokyo (dopo il professore) è il mio personaggio preferito, ma perché condivido pienamente il messaggio della battuta. Come si dice “se qualcuno ti tocca la mente, ti ha già messo le mani dappertutto”😏 perché non c’è niente di più sexy e forte di un’intesa e di un’attrazione mentale. Per me questa è una frase iconica e non poteva non essere detta da Ursola Corbero: \"LA MASERATI\"🔥.",
+            "url": "https://www.youtube.com/embed/IoQegluP5rM"
+        },
+        {
+            "titolo": "SKAM Italia - Stag 1. / Ep 7.",
+            "descrizione": "L’avete vista skam? È uscita da poco la 4ª stagione😍. Skam è una serie d’origine norvegese che è stata riadattata e riprodotta in Spagna, Francia, Germania, Paesi Bassi e per fortuna anche Italia, dove ha fatto il botto. Per me è una serie pazzesca: ti intrattiene ma allo stesso tempo ti fa soprattutto riflettere, perché riesce a trattare temi delicati in modo leggero, rendendoli così chiari e alla portata di tutti. Io ho scelto di riproporvi questa scena interpretata da Benedetta Gargari e Giancarlo Commare , non solo perché sono la mia coppia preferita, ma perché lei prende una posizione e difende una sua amica e in questo atteggiamento mi ci sono ritrovata molto.🥰 Ho sempre preso le difese dei più deboli, sostenuto le persone a me care e lottato contro i pregiudizi e i soprusi. Chi mi conosce lo sa bene che, anche a costo di rimetterci io, non posso tacere o non agire in alcune situazioni.😅 Fatemi sapere nei commenti quale è la vostra coppia preferita, cosa pensate della serie e della mia interpretazione.👇🏼 Grazie per sostenermi sempre, non c’è cosa più importante per me, davvero grazie mille e #sognateconme🌟 Spero anche io di poter far presto parte di un cast meraviglioso come questo🤞🏻.",
+            "url": "https://www.youtube.com/embed/BrF4C9Az2d0"
+        },
+        {
+            "titolo": "Monologo tratto dal film \"I love shopping\"",
+            "descrizione": "Interpretazione personale del breve monologo iniziale tratto dal film americano “I love shopping” (2009) con la splendida Isla Fisher. Per me questa commedia è pura verità: le donne e lo shopping sono una cosa sola.😂🛍🤑 Mi piace interpretare i monologhi a modo mio, cioè farli miei senza copiare pari pari il film, perché vorrei fare uscire la mia personalità. L’avete visto? Cosa ne pensate?",
+            "url": "https://www.youtube.com/embed/SE6R4oYGy5s"
+        },
+        {
+            "titolo": "Monologo tratto dal film \"L'amore non va in vacanza\"",
+            "descrizione": "Questa è una mia interpretazione personale del monologo tratto dal film \"L’amore non va in vacanza\". Commedia romantica americana con le meravigliose Cameron Diaz & Kate Winslet. Questa scena mi sta molto a cuore. In psicologia si dice che la fine di una storia d’amore causata da un evento traumatico, come il tradimento, viene vissuta e rielaborata come un lutto. Un tradimento, soprattutto nelle donne e nella maggior parte delle situazioni, crea delle dinamiche che inducono insicurezza e problemi di autostima (più di quelli che di norma noi donne complessate abbiamo), e sensi di colpevolezza. Ci tengo tantissimo a trasmettere questo messaggio: se il “vostro amore” vi tradisce “il problema NON siete voi” e NON è assolutamente colpa vostra, perché il tradimento NON ha giustificazioni: CHI AMA DAVVERO NON TRADISCE!!! Purtroppo oggi viviamo in un mondo in cui la fiducia è una qualità quasi estinta e sono sempre più rare le persone che amano davvero e sanno cosa significa vivere una relazione basata sul rispetto. Queste situazioni mi rattristano molto e mi fanno pensare tanto...Per questo...se tu che stai leggendo stai attraverso ora questo terrificante e infelice momento vorrei dirti che: Se tu hai amato davvero non devi avere rimpianti e/o ripensamenti, anzi devi essere felice, perché amare è meraviglioso ed è una cosa per pochi. Purtroppo però, il male che stai provando ora non passerà mai, imparerai a conviverci e questo con il tempo ti renderà più forte! Ricordati anche questa grande citazione: “ciò che non ti uccide ti fortifica”. So che adesso tu ti senti letteralmente morire dentro, ed è normale perché succede questo prima di rinascere e ricominciare a vivere. ORA: Riparti da te, ritrova la tua libertà e rivaluta la tua felicità, perché non è assolutamente tra le braccia di chi ti ha ferito e preso in giro! Vuoi sapere quale è stato il mio segreto? Ho imparato egoisticamente ad amarmi incondizionatamente.❤️",
+            "url": "https://www.youtube.com/embed/Pa6eWmiC5_I"
+        }
+    ]
+}
+
+archivio.video.forEach(video => {
+    let slide = '<div class="carousel-item">' +
+        '<iframe class="d-block col-10 col-md-7 mx-auto youtube_video_player" width="560" height="315" src="' + video.url + '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' +
+        '<div class="carousel-caption d-none d-md-block">' +
+        '    <h5>' + video.titolo + '</h5>' +
+        '    <p>' + video.descrizione + '</p>' +
+        '</div></div>';
+    $('.carousel-inner').append(slide)
+
+});
+$('.carousel-item').eq(0).addClass('active');
